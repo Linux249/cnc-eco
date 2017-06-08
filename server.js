eeda93fc49e594c307bd78ac8bc61a42fad5d7a5
julian <julian.libor@gmail.com>
2017-06-08 20:40:29 +0200

    update port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({
     host: 'localhost',
-    port: 8000,
+    port: (process.env.PORT || 5000),
     routes: { cors: false }
 });
 
