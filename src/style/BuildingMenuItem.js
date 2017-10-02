c8b06763b56c479b9de1e613e05bbadc52d664f0
julian <julian.libor@gmail.com>
2017-10-02 16:34:26 +0200

    add Building Menu Item

---
+++ src/style/BuildingMenuItem.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export default styled.div`
+    display: inline-block;
+    width: 25%;
+    height: 15%;
+    align-content: center;
+    text-align: center;
+  
+    & > img {
+    /*height: auto;*/
+    max-width: 100%;
+    max-height: 100%;
+    }
+`
