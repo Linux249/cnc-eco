6b3fe8e4e906ddf421bcce0bc6265559f29db6cb
julian <julian.libor@gmail.com>
2017-10-09 14:52:49 +0200

    add cors header

--- src/app.js
+++ src/app.js
@@ -8,16 +8,28 @@ import apiRouter from "./routes/index"
 //const config = require('./env.json')[process.env.NODE_ENV || 'development']
 
 
-/*import test from './test/Literatur'
-test()*/
-
-//const putMillionEntrys = require('./test')
-//putMillionEntrys(10000000)
-
 
 // Create the express app
 const app = express()
 
+app.use((req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH,DELETE')
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    // Pass to next layer of middleware
+    next()
+})
 
 //zeigt verschiedene logs in der Console an
 //app.use(logger("dev"))
