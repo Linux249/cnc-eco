09285a43ecd363b261a341b27baffb9743701a40
julian <julian.libor@gmail.com>
2017-10-09 17:32:32 +0200

    add inputs to layouts

--- src/components/Layouts.js
+++ src/components/Layouts.js
@@ -2,22 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import Body from '../style/Body'
 import Title from '../style/Title'
-import Button from '../style/Button'
-import Row from '../style/Row'
+// import Button from '../style/Button'
+// import Row from '../style/Row'
 import Layout from './Layout'
-import Login from './Login'
+
 
 class BaseHeader extends Component
 {
     constructor(props) {
         super()
         this.state = {
-            layouts: []
+            layouts: [],
+            pl: "linux249",
+            a: 126,
+            w: 373
         }
     }
-    componentWillMount(props) {
+    componentWillMount() {
         //get layouts from api
-        fetch(" http://cnc-eco.herokuapp.com/api/v1/layouts?pl=linux249&w=373&a=126")
+        const { pl, w, a } = this.props
+        fetch(`https://cnc-eco.herokuapp.com/api/v1/layouts?pl=${pl}&w=${w}&a=${a}`)
             .then(res => res.json())
             .then(layouts => {
                 console.log(layouts)
@@ -30,6 +34,9 @@ class BaseHeader extends Component
         const { layouts } = this.state
         return (
             <Body>
+            <input value={this.state.pl}/>
+            <input value={this.state.w} />
+            <input value={this.state.a}/>
                 <Title>{layouts.length}</Title>
                 {layouts.map((layout, i) => <Layout key={i} layout={layout}/>)}
 
@@ -39,5 +46,12 @@ class BaseHeader extends Component
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        pl: state.player.pl,
+        w: state.player.w,
+        a: state.player.a
+    }
+}
 
-export default BaseHeader
+export default connect(mapStateToProps)(BaseHeader)
