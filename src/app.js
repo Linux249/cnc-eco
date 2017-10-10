4dfb28f03b51e584bd18de5936c9396027c27c64
julian <julian.libor@gmail.com>
2017-10-10 16:41:40 +0200

    add cors stuffs

--- src/app.js
+++ src/app.js
@@ -32,7 +32,8 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
 
     // Website you wish to allow to connect
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    // DEV
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
     //res.setHeader('Access-Control-Allow-Origin', 'https://prodgame08.alliances.commandandconquer.com/')
 
     // Request methods you wish to allow
