14f95fd0aeaca795f0c3c7ecc47c574bf420ab14
julian <julian.libor@gmail.com>
2017-10-12 18:56:20 +0200

    ad action/reducer for upgrading many building

--- src/actions/base.js
+++ src/actions/base.js
@@ -3,6 +3,8 @@ import {copyObj} from '../services/util'
 export const REPLACE_BUILDING = "REPLACE_BUILDING" // ad a new building a current location
 export const REPLACE_ALL_BASE = "REPLACE_ALL_BASE" // ad a new building a current location
 export const CHANGE_FRACTION = "CHANGE_FRACTION"
+export const BUILDING_TYP_UP = "BUILDING_TYP_UP"
+export const BUILDING_TYP_DOWN = "BUILDING_TYP_DOWN"
 
 // number where the building is set is inside building.slot
 export function replaceBuilding(building) {
@@ -24,6 +26,21 @@ export function changeFraction(faction) {
     return {
         type: CHANGE_FRACTION,
         faction
+    }
+}
+
+export function buildingsUp(building) {
+    console.log("KLICK FUNKTIONIERT!")
+    return {
+        type: BUILDING_TYP_UP,
+        building
+    }
+}
+
+export function buildingsDown(building) {
+    return {
+        type: CHANGE_FRACTION,
+        building
     }
 }
 
