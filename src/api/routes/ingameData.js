424954221d8a5994a5ab4479c9134cbe8426ff50
julian <julian.libor@gmail.com>
2017-10-16 11:24:55 +0200

    delete copy paste content

---
+++ src/api/routes/ingameData.js
@@ -0,0 +1,25 @@
+'use strict'
+import { Router } from "express"
+const router = Router()
+
+
+// GET /api/v1/layout
+// get a single labtop with world + coords as params
+router.post("/ingameData", (req, res, next) => {
+    const { w, x,y } = req.query
+    // TODO auth require
+
+    const collection = req.db.collection(`layouts_${w}`)
+    collection.findOne({x, y},
+        (err, layout) => {
+            if(err) {
+                console.log(err)
+                next(err)
+            }
+            //console.log(`GET:\t${collection.namespace} - items: ${layout.length}`)
+            res.json(layout)
+        }
+    )
+
+    res.send("UPDATED")
+})
