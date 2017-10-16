02272462de112bc2a63895ec2003d8013b629945
julian <julian.libor@gmail.com>
2017-10-16 11:41:07 +0200

    test ingameData

--- src/api/routes/ingameData.js
+++ src/api/routes/ingameData.js
@@ -8,18 +8,19 @@ const router = Router()
 router.post("/ingameData", (req, res, next) => {
     const { w, x,y } = req.query
     // TODO auth require
+    console.log(req.body)
 
-    const collection = req.db.collection(`layouts_${w}`)
-    collection.findOne({x, y},
-        (err, layout) => {
-            if(err) {
-                console.log(err)
-                next(err)
-            }
-            //console.log(`GET:\t${collection.namespace} - items: ${layout.length}`)
-            res.json(layout)
-        }
-    )
-
+    // const collection = req.db.collection(`layouts_${w}`)
+    // collection.findOne({x, y},
+    //     (err, layout) => {
+    //         if(err) {
+    //             console.log(err)
+    //             next(err)
+    //         }
+    //         //console.log(`GET:\t${collection.namespace} - items: ${layout.length}`)
+    //         res.json(layout)
+    //     }
+    // )
+    //
     res.send("UPDATED")
 })
