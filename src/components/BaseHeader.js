469cc661a245e7ad287aa5eec4288bf7c8ce7c75
julian <julian.libor@gmail.com>
2017-11-15 23:36:47 +0100

    trying to change style for active links

--- src/components/BaseHeader.js
+++ src/components/BaseHeader.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import Header from '../style/BaseHeader'
 import Title from '../style/Title'
 import Button from '../style/Button'
 import Row from '../style/Row'
 import UrlInfo from './UrlInfo'
 import Login from './Login'
+import s from 'styled-components'
+
+const Link = s(NavLink)`
+    height: 50px;
+    
+    &.active {
+        back-ground-color: black;
+        color: black;
+    }
+`
+// https://github.com/styled-components/styled-components/issues/184
 
 class BaseHeader extends Component
 {
@@ -17,13 +28,13 @@ class BaseHeader extends Component
                 <UrlInfo />
                 <Row>
                     <Button>
-                        <Link to="/bases">Basen</Link>
+                        <Link  to="/bases" activeClassName="active">Basen</Link>
                     </Button>
                     <Button>
-                        <Link to="/scripts">Scripte</Link>
+                        <Link  to="/scripts" activeClassName="active">Scripte</Link>
                     </Button>
                     <Button>
-                        <Link to="/layouts">Layouts</Link>
+                        <Link  to="/layouts" activeClassName="active">Layouts</Link>
                     </Button>
                     <Login />
                 </Row>
