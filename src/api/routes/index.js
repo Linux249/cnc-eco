7b0d1efe598ac06a44c8f8140591369f31a06967
julian <julian.libor@gmail.com>
2018-07-05 13:08:14 +0200

    add alliance to router

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -6,12 +6,14 @@ const router = Router()
 import layouts from "./layouts"
 import db from "./db"
 import player from "./player"
+import alliance from "./alliance"
 import ingameData from "./ingameData"
 
 
 router.use("/", layouts)
 router.use("/", db)
 router.use("/", player)
+router.use("/", alliance)
 router.use("/", ingameData)
 
 export default router;
