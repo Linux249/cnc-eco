15dd353bb8f6dc108b5a7819be5981e1296737fb
julian <julian.libor@gmail.com>
2018-07-05 12:59:15 +0200

    add get alliance info route

---
+++ src/api/routes/alliance.js
@@ -0,0 +1,41 @@
+'use strict'
+import { Router } from "express"
+const router = Router()
+import Alliance from '../model/Alliance'
+
+// GET // api/v1/alliance?world=22&alliance=123
+router.get("/alliance", async (req, res, next) => {
+    console.log("Test logs in /alliance")
+    const { world } = req.query
+    const allianceId = req.query.alliance
+
+    if(!world) next(new Error("world id is missing"))
+    if(!allianceId) next(new Error("alliance id is missing"))
+
+    const aId = Number(`${allianceId}${world}`)
+
+    console.log({ world, allianceId, aId })
+
+    const alliance = await Alliance.findOne({allianceId: aId})
+
+    if(!alliance) next(new Error("No valid alliance id"))
+
+    // WORLD OF PLAYER
+    const collection = db.collection(`players_${world}`)
+
+    await new PromiseAll(alliance.members.map(async (member, i) => {
+        // TODO was wen kein player gefunden wird?
+        const { playerId } = member
+        const player = await collection.findOne({playerId})
+        // update player
+        aliance.members[i] = {...member, ...player}
+    })
+
+
+    // TODO auth require
+
+    res.json(aliance)
+})
+
+
+export default router
