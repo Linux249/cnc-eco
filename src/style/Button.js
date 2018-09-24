50353720e0bc0475ddcc4372bcf66e3f765b49bf
julian <julian.libor@gmail.com>
2018-09-24 15:22:56 +0200

    add click effect

--- src/style/Button.js
+++ src/style/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { shadow, backgroundColor, border, borderRadius } from './constants'
 
-export default styled.div`
+export const Button = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -19,5 +19,10 @@ export default styled.div`
     &:hover {
         box-shadow: 0 4px 8px 0 rgba(30, 136, 229, 0.2), 0 6px 20px 0 rgba(30, 136, 229, 0.19);
     }
+    
+    &:active {
+         box-shadow: 0 2px 10px rgba(150, 150, 150, 0.1)
+    }
 
 `
+export default Button
