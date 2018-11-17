fc21844c160ebb87a3705f320a1cff428599cde1
julian <julian.libor@gmail.com>
2018-11-17 20:48:09 +0100

    deploy

--- cnc-eco/service-worker.js
+++ cnc-eco/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cnc-eco/precache-manifest.bcafc88daf4c85f11210c8180e4b60be.js"
+  "/precache-manifest.5e54262320aaf0ef6d25bf37d827e86a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cnc-eco/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
