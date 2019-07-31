c5704e47528b6eb48d109899b57bd9016e4f1500
julian libor <julian.libor@gmail.com>
2019-07-31 19:15:18 +0200

    fix private route

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -3,12 +3,24 @@ import { Redirect, Route } from 'react-router-dom';
 import { LOCAL_STORE } from '../config';
 
 export default props => {
-    let { auth, path, name, component } = props;
+    let { auth, name, component: Component, ...rest } = props;
     const data = JSON.parse(localStorage.getItem(LOCAL_STORE));
     if (!auth && data) auth = data.token && data.user.isVerified;
     if (!name && data) name = !!data.user.player;
-    if (auth) {
-        if (name || path === '/user') return <Route path={path} component={component} />;
-        else return <Redirect to="/user" />;
-    } else return <Redirect to="/login" />;
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                auth ? (
+                    name || rest.path === '/user' ? (
+                        <Component {...props} />
+                    ) : (
+                        <Redirect to="/user" />
+                    )
+                ) : (
+                    <Redirect to="/login" />
+                )
+            }
+        />
+    );
 };
