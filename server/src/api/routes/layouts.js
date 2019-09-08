877baf51f80395d939fe82c23f85af0aad02bba7
julian libor <julian.libor@gmail.com>
2019-09-08 22:31:37 +0200

    try to fix 15:00 bug

--- server/src/api/routes/layouts.js
+++ server/src/api/routes/layouts.js
@@ -6,40 +6,45 @@ import { layoutStats } from '../utils/layout';
  * - the client protecting to get layouts from other worlds
  */
 export default async (req, res, next) => {
-    res.send(); // todo layouts update send empty body, check what happens if they send something back
+    try {
+        res.send(); // todo layouts update send empty body, check what happens if they send something back
 
-    let { db, body, headers, query } = req;
-    const { w, pl, a } = query;
-    if (headers['content-type'].includes('text')) body = JSON.parse(body);
+        let { db, body, headers, query } = req;
+        const { w, pl, a } = query;
+        if (headers['content-type'].includes('text')) body = JSON.parse(body);
 
-    const layouts = Object.keys(body).map(key => {
-        const [x, y] = key.split(':');
-        const layoutString = body[key].layout.slice(0, 72);
-        const { tib, cris, power, powerLayout } = layoutStats(layoutString);
-        return {
-            x: +x,
-            y: +y,
-            level: body[key].level,
-            alliance: +a,
-            // world: +w,
-            player: pl,
-            layout: layoutString,
-            time: new Date(),
-            tib,
-            cris,
-            power,
-            powerLayout,
-        };
-    });
-    const collection = db.collection(`layouts_${w}`);
-    console.log(`UPDATE LAYOUTS: ${layouts.length}# from ${pl}(${a}) on ${w}`);
-
-    await layouts.forEach(layout => {
-        const filter = { x: +layout.x, y: +layout.y, alliance: +a };
-        if (+a === 0) filter.player = pl;
-        collection.updateOne(filter, layout, { upsert: true }, err => {
-            if (err) next(err);
-            // console.log(result.result);
+        const layouts = Object.keys(body).map(key => {
+            const [x, y] = key.split(':');
+            const layoutString = body[key].layout.slice(0, 72);
+            const { tib, cris, power, powerLayout } = layoutStats(layoutString);
+            return {
+                x: +x,
+                y: +y,
+                level: body[key].level,
+                alliance: +a,
+                // world: +w,
+                player: pl,
+                layout: layoutString,
+                time: new Date(),
+                tib,
+                cris,
+                power,
+                powerLayout,
+            };
         });
-    });
+        const collection = db.collection(`layouts_${w}`);
+        console.log(`UPDATE LAYOUTS: ${layouts.length}# from ${pl}(${a}) on ${w}`);
+
+        await layouts.forEach(layout => {
+            const filter = { x: +layout.x, y: +layout.y, alliance: +a };
+            if (+a === 0) filter.player = pl;
+            collection.updateOne(filter, layout, { upsert: true }, err => {
+                if (err) next(err);
+                // console.log(result.result);
+            });
+        });
+    } catch (e) {
+        console.error(e)
+        next(e)
+    }
 };
