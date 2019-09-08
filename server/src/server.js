43e15585ac209a4d479040972f12714d77b0890f
julian libor <julian.libor@gmail.com>
2019-09-08 12:28:50 +0200

    add redirect to https everything

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import path from 'path';
+
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, { pingInterval: 1000 });
 const api = require('./api/app');
-
 const PORT = process.env.PORT || 8000; //: 4444;
 
 // app.use((req, res, next) => {
@@ -57,18 +57,25 @@ io.on('connect', socket => {
     });
 });
 
+/** add the api to express*/
 app.use('/api/v1', api);
-// Start the server
-// server.start((err) => {
-//
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('Server running at:', server.info.uri);
-// });
 
 /**
- * add the public folder
+ * redicrect all to https
+ * todo: this is bedder before adding the app but check out if there is a source which calls api without https first!
+ */
+if (process.env.NODE_ENV === 'production') {
+    app.enable('trust proxy'); // thats needed for having the
+    app.use(function(req, res, next) {
+        if (!req.secure) {
+            return res.redirect(`https://${req.get('Host')}${req.url}`);
+        }
+        next();
+    });
+}
+
+/**
+ * add the public folder for server SPA
  */
 const p = path.join(__dirname, '../../client/build');
 app.use('/', express.static(p));
@@ -78,10 +85,10 @@ app.use('/', express.static(p));
 /*
 app.use(express.static(path.join(__dirname, 'public')));
 */
-
-app.use('/*', function (req, res) {
-    const file = path.join(p, 'index.html')
-    console.log('File loaded -route: ', req.url, '/',file)
+// todo that is maybe useless, check logs time to time
+app.use('/*', function(req, res) {
+    const file = path.join(p, 'index.html');
+    console.log('File loaded -route: ', req.url, '/', file);
     res.sendFile(file);
 });
 
@@ -94,4 +101,4 @@ app.listen(PORT, () => {
     console.log(`Server gestartet - Port: ${PORT}`);
 });
 
-export default app
+export default app;
