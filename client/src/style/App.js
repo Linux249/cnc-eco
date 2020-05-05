fd11a773812010edef46abca770ebad34c83aef8
julian libor <julian.libor@gmail.com>
2020-05-05 15:25:32 +0200

    fix: body height

--- client/src/style/App.js
+++ client/src/style/App.js
@@ -1,15 +1,13 @@
 import styled from 'styled-components';
 
 export default styled.div`
-    font-family: Montserrat, Arial, sans-serif;
+    min-height: 100%;
 
+    font-family: Montserrat, Arial, sans-serif;
     font-size: 0.8rem;
-    //color: #4d3e13;
     font-weight: 500;
-    //letter-spacing: 1px;
-    height: 100%;
-    
+
     display: grid;
-    grid-template-rows: min-content 4.8px min-content 1fr min-content; 
+    grid-template-rows: min-content 4.8px min-content 1fr min-content;
     grid-template-columns: 1fr;
 `;
