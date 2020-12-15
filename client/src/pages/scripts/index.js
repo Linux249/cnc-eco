e28d954054875c5a730c2cfe0ae079fbcab7cec2
Julian Libor <jl@nuuk.de>
2020-12-15 10:59:06 +0100

    refactor: /script page to var content

--- client/src/pages/scripts/index.js
+++ client/src/pages/scripts/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 import styled from 'styled-components';
-import { InfoText } from '../../style/InfoText';
+import { InfoText } from '@/style/InfoText';
 import Body from '../../style/Body';
 import Area from '../../style/Area';
 import Title from '../../style/Title';
+
 
 const Description = styled.div`
     font-weight: 300;
@@ -11,40 +13,37 @@ const Description = styled.div`
     margin: 5px;
 `;
 
-const Link = styled.div`
+const A = styled.a`
     margin: 0.5rem;
     font-weight: 600;
 `;
+
+const data = [{
+    title: 'C&C:TA CnC-Eco',
+    text: 'The \'CnC:TA CnC-Eco\' script provides the upload functionality for your ingame data and give you access to a base scanner witch save all scanned bases.',
+    href: 'https://greasyfork.org/en/scripts/33978-c-c-ta-cnc-eco',
+    info: 'This Script is necessary for full usage of cnc-eco.',
+}, {
+    title: 'CnC-Eco Link Button',
+    text: 'Easy sharing Bases with others. Create and directly open a Link with the information of the selected Base',
+    href: 'https://greasyfork.org/en/scripts/27660-c-c-ta-cnc-eco-link-button',
+    info: '',
+}];
 
 
 const Index = () => <Body>
     <div />
     <div>
-        <Area>
-            <Title>C&C:TA CnC-Eco</Title>
+        {data.map((e) => <Area>
+            <Title>{e.title}</Title>
             <Description>
-                The 'CnC:TA CnC-Eco' script provides the upload functionality for your ingame
-                data and give you access to a base scanner witch save all scanned bases.
+                {e.text}
             </Description>
-            <a target="_blank" href="https://greasyfork.org/en/scripts/33978-c-c-ta-cnc-eco">
-                <Link>https://greasyfork.org/en/scripts/33978-c-c-ta-cnc-eco</Link>
-            </a>
-            <InfoText>This Script is necessary for full usage of cnc-eco.</InfoText>
-        </Area>
-
-        <Area>
-            <Title>CnC-Eco Link Button</Title>
-            <Description>
-                Easy sharing Bases with others. Create and directly open a Link with the
-                information of the selected Base
-            </Description>
-            <a
-                target="_blank"
-                href="https://greasyfork.org/en/scripts/27660-c-c-ta-cnc-eco-link-button"
-            >
-                <Link>https://greasyfork.org/en/scripts/27660-c-c-ta-cnc-eco-link-button</Link>
-            </a>
-        </Area>
+            <Link href={e.href}>
+                <A target='_blank'>{e.href}</A>
+            </Link>
+            <InfoText>{e.info}</InfoText>
+        </Area>)}
     </div>
     <div />
 </Body>;
